6599df14c4a8d3c56ac19243add2994f4f5596b5
AaronOT <55807823+AaronOT@users.noreply.github.com>
2020-03-27 16:25:33 +0100

    Modificacion de asistencias.proc

--- services/asistencia.proc.php
+++ services/asistencia.proc.php
@@ -12,16 +12,10 @@
 	$hora=date('H:i:s');
 
 
-	//$fecha=date('d-m-Y');
-	//echo $hora;
-
-
 
 	//Array con los dias de la semana
 
-	$dias = array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
-
-	//echo "Buenos días, hoy es ".$dias[date("N")];
+	$dias = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 
 
 
@@ -37,21 +31,60 @@
 	$row = mysqli_fetch_array($result);
     $id_classe=$row['id_classe'];
 
-	
-	
 
-	//echo "SELECT * FROM tbl_classe_uf INNER JOIN tbl_edicio_uf ON tbl_classe_uf.id_ed_uf=tbl_edicio_uf.id_ed_uf WHERE id_usuari=$id_usuari";
-
-	echo $row['hora_ini_classe'] ;
 
 	//Si la hora actual esta entre la hora de inicio y final de la clase, y el dia actual tambien coincide, hace el insert en la base de datos
+
+	$nuevafecha = strtotime ( '+5 minute' , strtotime ( $row['hora_ini_classe'] ) ) ;
+	$nuevafecha1 = strtotime ( '+10 minute' , strtotime ( $row['hora_ini_classe'] ) ) ;
+
+	//Hora de la base de datos +5 minutos
+	$hora1 = date('H:i:s' , $nuevafecha );
+	//Hora de la base de datos +10 minutos
+	$hora2 = date('H:i:s' , $nuevafecha1 );
+
+
+	echo $hora1;
+
+	echo "<br>";
+
+	echo $hora2;
+
 
 
 	if ($hora>$row['hora_ini_classe'] && $hora<$row['hora_fi_classe'] && $dias[date("N")]=$row['dia_classe']) {
 		
-		$result1 = mysqli_query($conn, "INSERT INTO `tbl_assist_classe`(`hora_assist_classe`, `present`, `id_usuari`, `id_classe`) VALUES ('".$hora."',1,".$id_usuari.",".$id_classe.")");
 
-		echo "BIENNNNNNNNNNN";
+
+		if ($hora>$row['hora_ini_classe'] && $hora<$hora1) {
+			//A la hora 
+
+
+		$result1 = mysqli_query($conn, "INSERT INTO `tbl_assist_classe`(`hora_assist_classe`, `data_assist_classe`, `present`, `retras`, `id_usuari`, `id_classe`) VALUES ('".$hora."','".$dia."',1,0,".$id_usuari.",".$id_classe.")");
+		
+
+		echo "A LA HORA";
+
+		}else if ($hora>$hora1 && $hora<$hora2) {
+			//retard
+
+		$result1 = mysqli_query($conn, "INSERT INTO `tbl_assist_classe`(`hora_assist_classe`, `data_assist_classe`, `present`, `retras`, `id_usuari`, `id_classe`) VALUES ('".$hora."','".$dia."',1,1,".$id_usuari.",".$id_classe.")");
+		
+		echo "RETARD";
+
+	}else{
+		//Falta
+
+		$result1 = mysqli_query($conn, "INSERT INTO `tbl_assist_classe`(`hora_assist_classe`, `data_assist_classe`, `present`, `retras`, `id_usuari`, `id_classe`) VALUES ('".$hora."','".$dia."',0,0,".$id_usuari.",".$id_classe.")");
+
+		echo "FALTA";
+	}
+		
+		
+
+	}else{
+
+		echo "Hoy es Sabado o Domingo, o ha ocurrido un error";
 	}
 
 
